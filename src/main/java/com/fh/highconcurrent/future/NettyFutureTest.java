79716eca4535e0b75887ee1ec375a4420ab284ee
fanhao <17710370457@163.com>
2022-01-07 09:53:00 +0800

    future学习3

---
+++ src/main/java/com/fh/highconcurrent/future/NettyFutureTest.java
@@ -0,0 +1,13 @@
+package com.fh.highconcurrent.future;
+
+/**
+ * @author hao.fan
+ * @dateTime 2022/1/5 8:49 下午
+ * @description
+ */
+public class NettyFutureTest {
+
+    public static void main(String[] args) {
+        // 见netty代码
+    }
+}
